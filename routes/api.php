00b9ea699186c20e8aee14fe1c3d87734eeb8a9a
Hafez300 <ziadwhafez@gmail.com>
2024-03-12 15:35:37 +0200

    review api with validate|ziad

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
 });
+
+
+
+Route::get("/hello", function(){
+    return "hello world";
+});
+
+Route::apiResource("reviews", ReviewController::class);
