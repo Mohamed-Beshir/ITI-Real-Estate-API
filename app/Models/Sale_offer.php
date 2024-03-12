8681791a73f3df9ab86c87c151d53ed6685e9ca3
Abdelrahman Ahmed <abdelrahman.elazizy.1999@gmail.com>
2024-03-12 21:09:37 +0200

    apifor sale offer and rent offer

---
+++ app/Models/Sale_offer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Property_sale;
+use App\Models\Sale_payment;
+
+class Sale_offer extends Model
+{
+    protected $fillable = ['buyer_id', 'property_sale_id', 'offered_price', 'message'];
+    protected $table = 'sales_offers';
+    use HasFactory;
+
+    public function property_sale()
+    {
+        return $this->belongsTo(Property_sale::class);
+    }
+
+    public function sale_payment()
+    {
+        return $this->belongsTo(Sale_payment::class);
+    }
+}
