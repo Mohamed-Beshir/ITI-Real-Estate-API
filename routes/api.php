c840128e09890b9cfa385b10cb119237ea9f9682
Mohamed Beshir <mohamedbeshir077@gmail.com>
2024-03-13 02:38:26 +0200

    register-login-auth-propertyList api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\api\auth\AuthController;
+
+use App\Http\Controllers\api\PropertyRentController;
+use App\Http\Controllers\api\PropertySaleController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +23,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('property_rents', PropertyRentController::class);
+    Route::apiResource('property_sales', PropertySaleController::class);
 });
