87a44aa6b191a89f259804c0e90123f65e542f56
Abdelrahman Ahmed <abdelrahman.elazizy.1999@gmail.com>
2024-03-24 05:56:41 +0200

    make mail to send messages to users gmail

--- app/Http/Controllers/RentOfferController.php
+++ app/Http/Controllers/RentOfferController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Rent_offer;
 use Illuminate\Support\Facades\Response;
+use Mail;
+use App\Mail\SendMail;
 
 
 class RentOfferController extends Controller
@@ -14,7 +16,18 @@ class RentOfferController extends Controller
         $offer = Rent_offer::findOrFail($id);
         $offer->status = 'accepted';
         $offer->save();
-
+        $buyer_name = $offer->buyer->name;
+        $buyer_email = $offer->buyer->email;
+        $propertyTitle = $offer->propertySale->property->title;
+        $mailData = [
+            'title' => 'Mail from BAZAR',
+            'body_1' => 'Your offer for ',
+            'body_2' => $propertyTitle,
+            'body_3' => ' has been accepted.',
+            'buyer_name' => $buyer_name,
+            'property_title' => $propertyTitle
+        ];
+        Mail::to($buyer_email)->send(new SendMail($mailData));
         return Response::json(['message' => 'Offer accepted successfully']);
     }
 
@@ -29,7 +42,18 @@ class RentOfferController extends Controller
         $offer = Rent_offer::findOrFail($id);
         $offer->status = 'rejected';
         $offer->save();
-
+        $buyer_name = $offer->buyer->name;
+        $buyer_email = $offer->buyer->email;
+        $propertyTitle = $offer->propertySale->property->title;
+        $mailData = [
+            'title' => 'Mail from BAZAR',
+            'body_1' => 'Your offer for ',
+            'body_2' => $propertyTitle,
+            'body_3' => ' has been rejected.',
+            'buyer_name' => $buyer_name,
+            'property_title' => $propertyTitle
+        ];
+        Mail::to($buyer_email)->send(new SendMail($mailData));
         return Response::json(['message' => 'Offer rejected successfully']);
     }
 }
--- app/Http/Controllers/SaleOfferController.php
+++ app/Http/Controllers/SaleOfferController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Sale_offer;
 use Illuminate\Support\Facades\Response;
+use App\Mail\SendMail;
+use Mail;
 
 
 class SaleOfferController extends Controller
@@ -14,7 +16,18 @@ class SaleOfferController extends Controller
         $offer = Sale_offer::findOrFail($id);
         $offer->status = 'accepted';
         $offer->save();
-
+        $buyer_name = $offer->buyer->name;
+        $buyer_email = $offer->buyer->email;
+        $propertyTitle = $offer->propertySale->property->title;
+        $mailData = [
+            'title' => 'Mail from BAZAR',
+            'body_1' => 'Your offer for ',
+            'body_2' => $propertyTitle,
+            'body_3' => ' has been accepted.',
+            'buyer_name' => $buyer_name,
+            'property_title' => $propertyTitle
+        ];
+        Mail::to($buyer_email)->send(new SendMail($mailData));
         return Response::json(['message' => 'Offer accepted successfully']);
     }
 
@@ -29,7 +42,18 @@ class SaleOfferController extends Controller
         $offer = Sale_offer::findOrFail($id);
         $offer->status = 'rejected';
         $offer->save();
-
+        $buyer_name = $offer->buyer->name;
+        $buyer_email = $offer->buyer->email;
+        $propertyTitle = $offer->propertySale->property->title;
+        $mailData = [
+            'title' => 'Mail from BAZAR',
+            'body_1' => 'Your offer for ',
+            'body_2' => $propertyTitle,
+            'body_3' => ' has been rejected.',
+            'buyer_name' => $buyer_name,
+            'property_title' => $propertyTitle
+        ];
+        Mail::to($buyer_email)->send(new SendMail($mailData));
         return Response::json(['message' => 'Offer rejected successfully']);
     }
 }
